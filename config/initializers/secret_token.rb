0014dfe9a577e804889d0220c0e2e7537baed8ba
unknown <marco.andreotti@pc-ti-22.srv_admin0.local>
2013-02-18 17:52:08 -0300

    Commitado.... Maluco mano

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teste::Application.config.secret_token = '0dc02b7a19c4758c74d9bc8988b4ba82fea36a28cb8bd7f8ac0bc1da85fb985289498f305d5e0e585922a368f037f79a31a1ea3e19d9e039467a9632998335d0'
